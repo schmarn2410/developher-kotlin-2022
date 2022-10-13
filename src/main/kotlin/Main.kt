105e8f745ac0980acd06d680061d7d64abc7cc89
Falk Woldmann <falk.woldmann@otto.de>
2022-10-13 17:09:01 +0200

    task 📝|add club checker

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,47 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Please Enter your name!")
+    val hi: String = readln()
+    println("Hello $hi!")
+    println("Checking whether you are on the guestlist...")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val guestList = listOf("Maria", "Blub")
+
+    // introduce class person later on
+
+    var isInvited: Boolean = false
+    for (guest in guestList) {
+        if (guest == hi) {
+            isInvited = true
+        }
+    }
+
+    if (!isInvited) { // or you can use !in
+        println("You are not invited!")
+        return
+    }
+
+    println("Welcome")
+
+    println("How old are you?")
+
+    val potentialAge = readln()
+    val age = getAge(potentialAge)
+
+    println("You are " + age + " old.") //println("You are $age old.")
+    if (age < 18) {
+        println("You are too young to enter!")
+        return
+    }
+
+    println("Passed!")
+}
+
+private fun getAge(potentialAge: String): Int {
+    while (true) {
+        try {
+            return potentialAge.toInt()
+        } catch (e: NumberFormatException) {
+            println("$potentialAge is no number. Try again with a valid number!")
+        }
+    }
 }
